9b4c2271e4b0663a56a9973082294bcc357c288e
Vladislav Pyatkin <vladiknator@gmail.com>
2024-03-07 11:38:52 -0700

    Save form to database when submitted

--- index.js
+++ index.js
@@ -33,6 +33,21 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use('/public', express.static('public'));
 
+async function doSQL(statement, params) {
+  const client = new pg.Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'notesdb',
+    user: 'postgres',
+    password: 'password',
+  });
+  await client.connect();
+  await client.query('SET TIME ZONE $1', ['UTC']);
+  const response = await client.query(statement, params);
+  await client.end();
+  return response;
+}
+
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -43,6 +58,39 @@ app.get('/about', (req, res) => {
 
 app.get('/new', (req, res) => {
   res.render('new');
+});
+
+app.post('/new', urlencodedParser, (req, res) => {
+  const newPost = {
+    title: req.body.title,
+    author: req.body.author,
+    oLId: req.body.oLId,
+    desc: req.body.desc,
+    stars: req.body.stars,
+    sDate: Date.parse(req.body.sDate),
+    fDate: Date.parse(req.body.fDate),
+    aDate: Date.now(),
+    notes: req.body.notes,
+  };
+
+  doSQL(
+    'INSERT INTO public.book(open_lib_id, author, title, description) VALUES ($1, $2, $3, $4);',
+    [newPost.oLId, newPost.author, newPost.title, newPost.desc],
+  );
+
+  doSQL(
+    'INSERT INTO public.note(open_lib_id, rating, date_started, date_finished, date_added, notes) VALUES ($1, $2, to_timestamp($3)::date, to_timestamp($4)::date, to_timestamp($5)::date, $6);',
+    [
+      newPost.oLId,
+      newPost.stars,
+      Math.floor(newPost.sDate.valueOf() / 1000),
+      Math.floor(newPost.fDate.valueOf() / 1000),
+      Math.floor(newPost.aDate.valueOf() / 1000),
+      newPost.notes,
+    ],
+  );
+
+  res.redirect('/');
 });
 
 app.listen(port, hostname, () => {
